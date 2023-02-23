d16aca774306c5afd93b22ef47ebd157f9b5e219
MartinJurisVisma <martin.juris@visma.com>
2023-02-23 17:02:51 +0100

    Adding Repo IRepo and one non functional test
    its too late

---
+++ ToDoAppBE/ToDoAppBE/Repository/IRepository/ITaskRepository.cs
@@ -0,0 +1,17 @@
+﻿using ToDoAppBE.DTOs;
+using ToDoAppBE.Entities;
+using ToDoAppBE.Model;
+
+namespace ToDoAppBE.Repository.IRepository;
+
+public interface ITaskRepository
+{
+    Task<List<TaskEntity>> GetAllTasksEntities();
+
+    Task<UserEntity> GetUserByName(string username);
+
+    Task<List<TaskEntity>> GetTaskByGroup(string group);
+
+    Task DeleteById(int id);
+
+}
---
+++ ToDoAppBE/ToDoAppBE/Repository/TaskRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.SignalR.Protocol;
+using Microsoft.EntityFrameworkCore;
+using ToDoAppBE.Database;
+using ToDoAppBE.DTOs;
+using ToDoAppBE.Entities;
+using ToDoAppBE.Model;
+using ToDoAppBE.Repository.IRepository;
+
+namespace ToDoAppBE.Repository;
+
+public class TaskRepository: ITaskRepository
+{
+    private readonly ApplicationContext _context;
+
+    public TaskRepository(ApplicationContext context)
+    {
+        _context = context;
+    }
+    
+   
+
+    public async Task<List<TaskEntity>> GetAllTasksEntities()
+    {
+        var entities = await _context.Tasks.ToListAsync();
+        
+        return entities;
+    }
+
+    public async Task<UserEntity> GetUserByName(string username)
+    {
+        var user = await _context.Users
+            .Include(x => x.Tasks)
+            .SingleOrDefaultAsync(x => x.Username == username);
+        return user;
+    }
+
+    public async Task<List<TaskEntity>> GetTaskByGroup(string group)
+    {
+        var items = await _context.Tasks.AsNoTracking().Where(x=>x.Group == group).ToListAsync();
+        
+        return items;
+    }
+
+    public async Task DeleteById(int id)
+    {
+        
+    }
+    
+    
+}
